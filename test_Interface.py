ff23e80e9781ef86282e17af08108db1d6d6767b
oslap <40290042+oslap@users.noreply.github.com>
2021-02-24 18:44:49 +0100

    classe test

---
+++ test_Interface.py
@@ -0,0 +1,54 @@
+import random
+from tkinter import *
+
+#On cree une fenetre et un canevas:
+Simulation = Tk()
+width=500
+height=500
+interface = Canvas(Simulation,width = width, height = height, bd=0, bg="white")
+interface.pack(padx=100,pady=100)
+interface.create_rectangle(10,10,width-10,height-10)
+
+#Creation  d'un bouton "Quitter":
+Bouton_Quitter=Button(Simulation, text ='Quitter', command = Simulation.destroy)
+#On ajoute l'affichage du bouton dans la fenêtre tk:
+Bouton_Quitter.pack()
+
+def CreaBalle():
+    xcoord = (width-30) * random.random()+10
+    ycoord = (height-30) * random.random()+10
+    return interface.create_oval(xcoord,ycoord,xcoord+20,ycoord+20,fill='red')
+
+dx = 10 * random.random()
+dy = 10 * random.random()
+
+def deplacement(b1):
+
+        global dx, dy
+        if interface.coords(b1)[3] > height-10:
+            dy=-dy
+        if interface.coords(b1)[3] < 30:
+            dy=-dy
+        if interface.coords(b1)[2] > width-10:
+            dx=-dx
+        if interface.coords(b1)[2] < 30:
+            dx=-dx
+
+        #On deplace la balle :
+        interface.move(b1,dx,dy)
+
+        #On repete cette fonction
+        Simulation.after(30,deplacement)
+
+
+
+#On cree une balle:
+#il faut connecter cette partie avec la classe People
+
+for i in range(0,1):
+    b1=CreaBalle()
+
+    deplacement(b1)
+#On lance la boucle principale:
+Simulation.mainloop()
+
