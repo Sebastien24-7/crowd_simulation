2ed7b28a2acc7356115242e2b6be88f42e870f27
Sébastien Vimont <sebastien.vimont@gmail.com>
2021-05-20 16:07:38 +0200

    Commit du modèle et de l'analyse (ajout dans l'excel) pour que tu puisses l'avoir et regarder avec moi

--- analyse.py
+++ analyse.py
@@ -1,4 +1,4 @@
-from numpy import *
+# from numpy import *
 import math
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
@@ -9,36 +9,58 @@ import pandas as pd
 # Variables
 
 def analyse():
-    # Variables avec Obstacle
-    xNbr_Obs_Ho_Al = []
-    yT_Obs_Ho_Al = []
+    xObs_Pos = []
+    yObs_Pos = []
+    Theo_yObs_Pos = []
+    xObs_Pos2 = []
+    yObs_Pos2 = []
+    Theo_yObs_Pos2 = []
+    xObs_Pos3 = []
+    yObs_Pos3 = []
+    Theo_yObs_Pos3 = []
+    xObs_Pos4 = []
+    yObs_Pos4 = []
+    Theo_yObs_Pos4 = []
 
-    xNbr_Obs_Ho_Ord = []
-    yT_Obs_Ho_Ord = []
+    # # Variables avec Obstacle
+    # xNbr_Obs_Ho_Al = []
+    # yT_Obs_Ho_Al = []
+    # Theo_yT_Obs_Ho_Al = []
+    #
+    # xNbr_Obs_Ho_Ord = []
+    # yT_Obs_Ho_Ord = []
+    # Theo_yT_Obs_Ho_Ord = []
+    #
+    # xNbr_Obs_Het_Al = []
+    # yT_Obs_Het_Al = []
+    # Theo_yT_Obs_Het_Al = []
+    #
+    # xNbr_Obs_Het_Ord = []
+    # yT_Obs_Het_Ord = []
+    # Theo_yT_Obs_Het_Ord = []
+    #
+    # ### Variables sans Obstacle ###
+    #
+    # xNbr_Wo_Ho_Al = []
+    # yT_Wo_Ho_Al = []
+    # Theo_yT_Wo_Ho_Al = []
+    #
+    # xNbr_Wo_Ho_Ord = []
+    # yT_Wo_Ho_Ord = []
+    # Theo_yT_Wo_Ho_Ord = []
+    #
+    # xNbr_Wo_Het_Al = []
+    # yT_Wo_Het_Al = []
+    # Theo_yT_Wo_Het_Al = []
+    #
+    # xNbr_Wo_Het_Ord = []
+    # yT_Wo_Het_Ord = []
+    # Theo_yT_Wo_Het_Ord = []
 
-    xNbr_Obs_Het_Al = []
-    yT_Obs_Het_Al = []
-
-    xNbr_Obs_Het_Ord = []
-    yT_Obs_Het_Ord = []
-
-    # Variables sans Obstacle
-
-    xNbr_Wo_Ho_Al = []
-    yT_Wo_Ho_Al = []
-
-    xNbr_Wo_Ho_Ord = []
-    yT_Wo_Ho_Ord = []
-
-    xNbr_Wo_Het_Al = []
-    yT_Wo_Het_Al = []
-
-    xNbr_Wo_Het_Ord = []
-    yT_Wo_Het_Ord = []
-
-    plt.title("Graphique des différentes modélisation (Temps en fonction Nbr)")
+    plt.title(
+        "Graphique des différentes modélisation (Temps en fonction Nbr)" '\n' "Avec Obstacle - Egoïste - N20 à 70" '\n' "Variation taille de la porte et population")
     plt.grid(True)
-    plt.xlabel("Population (Nombre d'individus)")
+    plt.xlabel("Taille de la porte")
     plt.ylabel("Temps d'évacuation")
 
     ### USE PANDAS
@@ -112,90 +134,187 @@ def analyse():
     loc = (r"C:\Users\sebas\PycharmProjects\crowd_simulation\crowd_analysis.xls")
     # To open Workbook
     wb = xlrd.open_workbook(loc)
-    sheet = wb.sheet_by_index(0)
+    sheet = wb.sheet_by_index(3)
 
     # For row 0 and column 0
     print(str(sheet.cell_value(0, 0)) + " Using xlrd")
 
     for i in range(1, sheet.nrows):
-        if str(sheet.cell_value(i, 6)) == "Oui":  # Avec Obstacle 'o'
-            if str(sheet.cell_value(i, 4)) == "Homogène":  # Population Homogène
-                if str(sheet.cell_value(i, 5)) == "Aléatoire":
-                    xNbr_Obs_Ho_Al.append(sheet.cell_value(i, 0))
-                    yT_Obs_Ho_Al.append(sheet.cell_value(i, 2))
 
-                    plt.plot(xNbr_Obs_Ho_Al, yT_Obs_Ho_Al, 'ob--', label="Obstacle-Homogène-Aléatoire")
-                    Obs_Ho_Al_patch = mlines.Line2D([], [], color="blue", marker='o', markersize=8,
-                                                    label="Obstacle-Homogène-Aléatoire")
+        if str(sheet.cell_value(i, 10)) == "Taille Porte":  # Variations Positions
+            if str(sheet.cell_value(i, 5)) == "Aléatoire":
+                if sheet.cell_value(i, 8) == 1:
+                    xObs_Pos.append(sheet.cell_value(i, 9))
+                    yObs_Pos.append(sheet.cell_value(i, 2))
+                    Theo_yObs_Pos.append(sheet.cell_value(i, 3))
 
-                    print(str(xNbr_Obs_Ho_Al) + " Using xlrd it works")
+                    plt.plot(xObs_Pos, yObs_Pos, 'or-', label="Temps Simulation N=50")
+                    Obs_Pos_patch = mlines.Line2D([], [], color="red", marker='o', markersize=8,
+                                                  label="Temps Simulation N=50")
+                    plt.plot(xObs_Pos, Theo_yObs_Pos, 'xr--', label="Temps Théorique N=50")
+                    Theo_Obs_Pos_patch = mlines.Line2D([], [], color="red", marker='x', markersize=8,
+                                                       label="Temps Théorique N=50")
+                if sheet.cell_value(i, 8) == 2:
+                    xObs_Pos2.append(sheet.cell_value(i, 9))
+                    yObs_Pos2.append(sheet.cell_value(i, 2))
+                    Theo_yObs_Pos2.append(sheet.cell_value(i, 3))
 
-                elif sheet.cell_value(i, 5) == "Ordonnée":
-                    xNbr_Obs_Ho_Ord.append(sheet.cell_value(i, 0))
-                    yT_Obs_Ho_Ord.append(sheet.cell_value(i, 2))
+                    plt.plot(xObs_Pos2, yObs_Pos2, 'ob-', label="Temps Simulation N=20")
+                    Obs_Pos2_patch = mlines.Line2D([], [], color="blue", marker='o', markersize=8,
+                                                   label="Temps Simulation N=20")
+                    plt.plot(xObs_Pos2, Theo_yObs_Pos2, 'xb--', label="Temps Théorique N=20")
+                    Theo_Obs_Pos2_patch = mlines.Line2D([], [], color="blue", marker='x', markersize=8,
+                                                        label="Temps Théorique N=20")
+                if sheet.cell_value(i, 8) == 3:
+                    xObs_Pos3.append(sheet.cell_value(i, 9))
+                    yObs_Pos3.append(sheet.cell_value(i, 2))
+                    Theo_yObs_Pos3.append(sheet.cell_value(i, 3))
 
-                    plt.plot(xNbr_Obs_Ho_Ord, yT_Obs_Ho_Ord, 'og--', label="Obstacle-Homogène-Ordonnée")
-                    Obs_Ho_Ord_patch = mlines.Line2D([], [], color="green", marker='o', markersize=10,
-                                                     label="Obstacle-Homogène-Ordonnée")
+                    plt.plot(xObs_Pos3, yObs_Pos3, 'oy-', label="Temps Simulation N=40")
+                    Obs_Pos3_patch = mlines.Line2D([], [], color="yellow", marker='o', markersize=8,
+                                                   label="Temps Simulation N=40")
+                    plt.plot(xObs_Pos3, Theo_yObs_Pos3, 'xy--', label="Temps Théorique N=40")
+                    Theo_Obs_Pos3_patch = mlines.Line2D([], [], color="yellow", marker='x', markersize=8,
+                                                        label="Temps Théorique N=40")
+                if sheet.cell_value(i, 8) == 4:
+                    xObs_Pos4.append(sheet.cell_value(i, 9))
+                    yObs_Pos4.append(sheet.cell_value(i, 2))
+                    Theo_yObs_Pos4.append(sheet.cell_value(i, 3))
 
-            elif sheet.cell_value(i, 4) == "Hétérogène":  # Population Hétérogène
-                if sheet.cell_value(i, 5) == "Aléatoire":
-                    xNbr_Obs_Het_Al.append(sheet.cell_value(i, 0))
-                    yT_Obs_Het_Al.append(sheet.cell_value(i, 2))
+                    plt.plot(xObs_Pos4, yObs_Pos4, 'og-', label="Temps Simulation N=70")
+                    Obs_Pos4_patch = mlines.Line2D([], [], color="green", marker='o', markersize=8,
+                                                   label="Temps Simulation N=70")
+                    plt.plot(xObs_Pos4, Theo_yObs_Pos4, 'xg--', label="Temps Théorique N=70")
+                    Theo_Obs_Pos4_patch = mlines.Line2D([], [], color="green", marker='x', markersize=8,
+                                                        label="Temps Théorique N=70")
 
-                    plt.plot(xNbr_Obs_Het_Al, yT_Obs_Het_Al, 'oy--', label="Obstacle-Hétérogène-Aléatoire")
-                    Obs_Het_Al_patch = mlines.Line2D([], [], color="yellow", marker='o', markersize=10,
-                                                     label="Obstacle-Hétérogène-Aléatoire")
+        # if str(sheet.cell_value(i, 4)) == "Homogène":  # Population Homogène
+        #     if str(sheet.cell_value(i, 5)) == "Aléatoire":
+        #         xNbr_Obs_Ho_Al.append(sheet.cell_value(i, 9))
+        #         yT_Obs_Ho_Al.append(sheet.cell_value(i, 2))
+        #         Theo_yT_Obs_Ho_Al.append(sheet.cell_value(i, 3))
+        #
+        #         plt.plot(xNbr_Obs_Ho_Al, yT_Obs_Ho_Al, 'ob--', label="Obstacle-Homogène-Aléatoire")
+        #         Obs_Ho_Al_patch = mlines.Line2D([], [], color="blue", marker='o', markersize=8,
+        #                                         label="Obstacle-Homogène-Aléatoire")
+        #         plt.plot(xNbr_Obs_Ho_Al, Theo_yT_Obs_Ho_Al, 'xb-', label="Théorique-Obs-Homogène-Aléatoire")
+        #         Theo_Obs_Ho_Al_patch = mlines.Line2D([], [], color="blue", marker='x', markersize=5,
+        #                                         label="Théorique-Obs-Homogène-Aléatoire")
+        #
+        #
+        #         print(str(xNbr_Obs_Ho_Al) + " Using xlrd it works")
+        #
+        #     elif sheet.cell_value(i, 5) == "Ordonnée":
+        #         xNbr_Obs_Ho_Ord.append(sheet.cell_value(i, 9))
+        #         yT_Obs_Ho_Ord.append(sheet.cell_value(i, 2))
+        #         Theo_yT_Obs_Ho_Ord.append(sheet.cell_value(i, 3))
+        #
+        #         plt.plot(xNbr_Obs_Ho_Ord, yT_Obs_Ho_Ord, 'og--', label="Obstacle-Homogène-Ordonnée")
+        #         Obs_Ho_Ord_patch = mlines.Line2D([], [], color="green", marker='o', markersize=8,
+        #                                          label="Obstacle-Homogène-Ordonnée")
+        #         plt.plot(xNbr_Obs_Ho_Ord, Theo_yT_Obs_Ho_Ord, 'xg-', label="Théorique-Obs-Homogène-Ordonnée")
+        #         Theo_Obs_Ho_Ord_patch = mlines.Line2D([], [], color="green", marker='x', markersize=5,
+        #                                         label="Théorique-Obs-Homogène-Ordonnée")
+        #
+        # elif sheet.cell_value(i, 4) == "Hétérogène":  # Population Hétérogène
+        #     if sheet.cell_value(i, 5) == "Aléatoire":
+        #         xNbr_Obs_Het_Al.append(sheet.cell_value(i, 9))
+        #         yT_Obs_Het_Al.append(sheet.cell_value(i, 2))
+        #         Theo_yT_Obs_Het_Al.append(sheet.cell_value(i, 3))
+        #
+        #         plt.plot(xNbr_Obs_Het_Al, yT_Obs_Het_Al, 'oy--', label="Obstacle-Hétérogène-Aléatoire")
+        #         Obs_Het_Al_patch = mlines.Line2D([], [], color="yellow", marker='o', markersize=8,
+        #                                          label="Obstacle-Hétérogène-Aléatoire")
+        #
+        #         plt.plot(xNbr_Obs_Het_Al, Theo_yT_Obs_Het_Al, 'xy-', label="Théorique-Obs-Hétérogène-Aléatoire")
+        #         Theo_Obs_Het_Al_patch = mlines.Line2D([], [], color="yellow", marker='x', markersize=5,
+        #                                         label="Théorique-Obs-Hétérogène-Aléatoire")
+        #
+        #
+        #     elif sheet.cell_value(i, 5) == "Ordonnée":
+        #         xNbr_Obs_Het_Ord.append(sheet.cell_value(i, 9))
+        #         yT_Obs_Het_Ord.append(sheet.cell_value(i, 2))
+        #         Theo_yT_Obs_Het_Ord.append(sheet.cell_value(i, 3))
+        #
+        #         plt.plot(xNbr_Obs_Het_Ord, yT_Obs_Het_Ord, 'or--', label="Obstacle-Hétérogène-Ordonnée")
+        #         Obs_Het_Ord_patch = mlines.Line2D([], [], color="red", marker='o', markersize=8,
+        #                                           label="Obstacle-Hétérogène-Ordonnée")
+        #
+        #         plt.plot(xNbr_Obs_Het_Ord, Theo_yT_Obs_Het_Ord, 'xr-', label="Théorique-Obs-Hétérogène-Ordonnée")
+        #         Theo_Obs_Het_Ord_patch = mlines.Line2D([], [], color="red", marker='x', markersize=5,
+        #                                         label="Théorique-Obs-Hétérogène-Ordonnée")
 
-
-                elif sheet.cell_value(i, 5) == "Ordonnée":
-                    xNbr_Obs_Het_Ord.append(sheet.cell_value(i, 0))
-                    yT_Obs_Het_Ord.append(sheet.cell_value(i, 2))
-
-                    plt.plot(xNbr_Obs_Het_Ord, yT_Obs_Het_Ord, 'or--', label="Obstacle-Hétérogène-Ordonnée")
-                    Obs_Het_Ord_patch = mlines.Line2D([], [], color="red", marker='o', markersize=10,
-                                                      label="Obstacle-Hétérogène-Ordonnée")
-
-
-        elif sheet.cell_value(i, 6) == "Non":  # Sans Obstacle 'v'
-            if sheet.cell_value(i, 4) == "Homogène":  # Population Homogène
-                if sheet.cell_value(i, 5) == "Aléatoire":
-                    xNbr_Wo_Ho_Al.append(sheet.cell_value(i, 0))
-                    yT_Wo_Ho_Al.append(sheet.cell_value(i, 2))
-
-                    plt.plot(xNbr_Wo_Ho_Al, yT_Wo_Ho_Al, 'vb--', label="SansObstacle-Homogène-Aléatoire")
-                    Sobs_Ho_Al_patch = mlines.Line2D([], [], color="blue", marker='v', markersize=10,
-                                                     label="SansObstacle-Homogène-Aléatoire")
-
-
-                elif sheet.cell_value(i, 5) == "Ordonnée":
-                    xNbr_Wo_Ho_Ord.append(sheet.cell_value(i, 0))
-                    yT_Wo_Ho_Ord.append(sheet.cell_value(i, 2))
-
-                    plt.plot(xNbr_Wo_Ho_Ord, yT_Wo_Ho_Ord, 'vg--', label="SansObstacle-Homogène-Ordonnée")
-                    Sobs_Ho_Ord_patch = mlines.Line2D([], [], color="green", marker='v', markersize=10,
-                                                      label="SansObstacle-Homogène-Ordonnée")
-
-
-            elif sheet.cell_value(i, 4) == "Hétérogène":  # Population Hétérogène
-                if sheet.cell_value(i, 5) == "Aléatoire":
-                    xNbr_Wo_Het_Al.append(sheet.cell_value(i, 0))
-                    yT_Wo_Het_Al.append(sheet.cell_value(i, 2))
-
-                    plt.plot(xNbr_Wo_Het_Al, yT_Wo_Het_Al, 'vy--', label="SansObstacle-Hétérogène-Aléatoire")
-                    Sobs_Het_Al_patch = mlines.Line2D([], [], color="yellow", marker='v', markersize=10,
-                                                      label="SansObstacle-Hétérogène-Aléatoire")
-
-
-                elif sheet.cell_value(i, 5) == "Ordonnée":
-                    xNbr_Wo_Het_Ord.append(sheet.cell_value(i, 0))
-                    yT_Wo_Het_Ord.append(sheet.cell_value(i, 2))
-
-                    plt.plot(xNbr_Wo_Het_Ord, yT_Wo_Het_Ord, 'vr--', label="SansObstacle-Hétérogène-Ordonnée")
-                    Sobs_Het_Ord_patch = mlines.Line2D([], [], color="red", marker='v', markersize=10,
-                                                       label="SansObstacle-Hétérogène-Ordonnée")
+        # if sheet.cell_value(i, 9) == "Laisse passer":  # Sans Obstacle 'v'
+        #     if sheet.cell_value(i, 4) == "Homogène":  # Population Homogène
+        #         if sheet.cell_value(i, 5) == "Aléatoire":
+        #             xNbr_Wo_Ho_Al.append(sheet.cell_value(i, 0))
+        #             yT_Wo_Ho_Al.append(sheet.cell_value(i, 2))
+        #             Theo_yT_Wo_Ho_Al.append(sheet.cell_value(i, 3))
+        #
+        #             plt.plot(xNbr_Wo_Ho_Al, yT_Wo_Ho_Al, 'vb--', label="SansObstacle-Homogène-Aléatoire")
+        #             Sobs_Ho_Al_patch = mlines.Line2D([], [], color="blue", marker='v', markersize=10,
+        #                                              label="SansObstacle-Homogène-Aléatoire")
+        #
+        #             plt.plot(xNbr_Wo_Ho_Al, Theo_yT_Wo_Ho_Al, 'xb-', label="Théorique-SansObs-Homogène-Aléatoire")
+        #             Theo_yT_Wo_Ho_Al_patch = mlines.Line2D([], [], color="blue", marker='x', markersize=5,
+        #                                              label="Théorique-SansObs-Homogène-Aléatoire")
+        #
+        #
+        #         elif sheet.cell_value(i, 5) == "Ordonnée":
+        #             xNbr_Wo_Ho_Ord.append(sheet.cell_value(i, 0))
+        #             yT_Wo_Ho_Ord.append(sheet.cell_value(i, 2))
+        #             Theo_yT_Wo_Ho_Ord.append(sheet.cell_value(i, 3))
+        #
+        #             plt.plot(xNbr_Wo_Ho_Ord, yT_Wo_Ho_Ord, 'vg--', label="SansObstacle-Homogène-Ordonnée")
+        #             Sobs_Ho_Ord_patch = mlines.Line2D([], [], color="green", marker='v', markersize=10,
+        #                                               label="SansObstacle-Homogène-Ordonnée")
+        #
+        #             plt.plot(xNbr_Wo_Ho_Ord, Theo_yT_Wo_Ho_Ord, 'xg-', label="Théorique-SansObs-Homogène-Ordonnée")
+        #             Theo_yT_Wo_Ho_Ord_patch = mlines.Line2D([], [], color="green", marker='x', markersize=5,
+        #                                              label="Théorique-SansObs-Homogène-Ordonnée")
+        #
+        #
+        #     elif sheet.cell_value(i, 4) == "Hétérogène":  # Population Hétérogène
+        #         if sheet.cell_value(i, 5) == "Aléatoire":
+        #             xNbr_Wo_Het_Al.append(sheet.cell_value(i, 0))
+        #             yT_Wo_Het_Al.append(sheet.cell_value(i, 2))
+        #             Theo_yT_Wo_Het_Al.append(sheet.cell_value(i, 3))
+        #
+        #
+        #             plt.plot(xNbr_Wo_Het_Al, yT_Wo_Het_Al, 'vy--', label="SansObstacle-Hétérogène-Aléatoire")
+        #             Sobs_Het_Al_patch = mlines.Line2D([], [], color="yellow", marker='v', markersize=10,
+        #                                               label="SansObstacle-Hétérogène-Aléatoire")
+        #
+        #             plt.plot(xNbr_Wo_Het_Al, Theo_yT_Wo_Het_Al, 'xy-', label="Théorique-SansObs-Hétérogène-Aléatoire")
+        #             Theo_yT_Wo_Het_Al_patch = mlines.Line2D([], [], color="yellow", marker='x', markersize=5,
+        #                                              label="Théorique-SansObs-Hétérogène-Aléatoire")
+        #
+        #
+        #         elif sheet.cell_value(i, 5) == "Ordonnée":
+        #             xNbr_Wo_Het_Ord.append(sheet.cell_value(i, 0))
+        #             yT_Wo_Het_Ord.append(sheet.cell_value(i, 2))
+        #             Theo_yT_Wo_Het_Ord.append(sheet.cell_value(i, 3))
+        #
+        #             plt.plot(xNbr_Wo_Het_Ord, yT_Wo_Het_Ord, 'vr--', label="SansObstacle-Hétérogène-Ordonnée")
+        #             Sobs_Het_Ord_patch = mlines.Line2D([], [], color="red", marker='v', markersize=10,
+        #                                                label="SansObstacle-Hétérogène-Ordonnée")
+        #
+        #             plt.plot(xNbr_Wo_Het_Ord, Theo_yT_Wo_Het_Ord, 'xr-', label="Théorique-SansObs-Hétérogène-Ordonnée")
+        #             Theo_yT_Wo_Het_Ord_patch = mlines.Line2D([], [], color="red", marker='x', markersize=5,
+        #                                              label="Théorique-SansObs-Hétérogène-Ordonnée")
 
     # plt.legend(loc="upper right")
-    plt.legend(handles=[Obs_Ho_Al_patch, Obs_Ho_Ord_patch, Obs_Het_Al_patch, Obs_Het_Ord_patch], loc="upper right")
-    # For next time just to put above : ,Sobs_Ho_Al_patch,Sobs_Ho_Ord_patch,Sobs_Het_Al_patch,Sobs_Het_Ord_patch
+    plt.legend(handles=[Obs_Pos_patch, Theo_Obs_Pos_patch, Obs_Pos2_patch, Theo_Obs_Pos2_patch, Obs_Pos3_patch,
+                        Theo_Obs_Pos3_patch, Obs_Pos4_patch, Theo_Obs_Pos4_patch], loc="best")
+
+    # plt.errorbar(x, y, xerr=0.1 * x, yerr=5.0 + 0.75 * y)
+    # For next time just to put above :
+    #                         ,Sobs_Ho_Al_patch,Sobs_Ho_Ord_patch,Sobs_Het_Al_patch,Sobs_Het_Ord_patch,
+    #                         Theo_yT_Wo_Ho_Al_patch,Theo_yT_Wo_Ho_Ord_patch,Theo_yT_Wo_Het_Al_patch,Theo_yT_Wo_Het_Ord_patch
+    #
+    #   Obs_Ho_Al_patch, Obs_Ho_Ord_patch, Obs_Het_Al_patch, Obs_Het_Ord_patch,
+    #   Theo_Obs_Ho_Al_patch, Theo_Obs_Ho_Ord_patch, Theo_Obs_Het_Al_patch, Theo_Obs_Het_Ord_patch
+
+    plt.autoscale(enable=True, axis='both', tight=None)
+    ###
     plt.show()
--- theory.py
+++ theory.py
@@ -28,31 +28,37 @@ from People import *
 #
 # t_total = max(tevac)  # Temps total de l'évacuation correspond à la valeur la plus élevée d'évacuation
 # print("Temps total théorique nécessaire pour sortir : " + str(t_total) + "secondes")
-
-def theory(N, w_porte, W_width, W_height, ListParticles, Dsortie):
+### Modèle revisité ###
+def theory(N, w_porte, W_width, W_height, ListParticles, coord_sortie):
     ### MODELE DE TOGAWA ###
     W = w_porte * 2  # Width of the door
     D = (int(N) / (
             (W_width / 3779) * (
             W_height / 3779)))  # Density of people by meter square (1 meter = 3779 pixels don't know from where)
 
-    v0 = 1.3 / 3779  # (m/s) vitesse de marche par défaut et sans effet de foule
+    v0 = 1.3  # (m/s) vitesse de marche par défaut et sans effet de foule
     vT = v0 * math.pow(D, -0.8)
 
     v = []  # speed of the individuals
-    for i in range(len(ListParticles)):
-        v.append(ListParticles[i].speed)
-    vAverage = mean(v)
-
-    F = vAverage * (W / 3779) * D  # Flux de personne (débit de pers / sec à l'ouverture)
-    Fspe = vAverage * D  # Flux de personne (débit spécifique en m^-1.s^-1 )
-
     tevac = []
-    L = []
+    L = []  # Distance de sortie normée
+    Dsortie = []  # Distance de sortie
+    Fspe = []
+    F = []
+
     for i in range(len(ListParticles)):
-        L.append(Dsortie / 3779)
-        tevac.append(((float(N) / (float(Fspe) * float(W / 3779))) + (
-                L[i] / vT)) * 10)  # Temps d'évacuation # LE FOIS 10 NE DOIT PAS ETRE LA
+        v.append(ListParticles[i].vit)
+    # vAverage = mean(v)
+
+    for i in range(len(ListParticles)):
+        F.append(v[i] * (W / 3779) * D)  # Flux de personne (débit de pers / sec à l'ouverture)
+        Fspe.append(v[i] * D)  # Flux de personne (débit spécifique en m^-1.s^-1 )
+        Dsortie.append(
+            ListParticles[i].Distance(coord_sortie))  # Pour obtenir la distance de sortie pour chaque particle
+
+        L.append(Dsortie[i] / 3779)
+        tevac.append(((float(N) / (float(Fspe[i]) * float(W / 3779))) + (
+                L[i] / vT)))  # Temps d'évacuation
 
     t_total = double(
         "{:.2f}".format(max(tevac)))  # Temps total de l'évacuation correspond à la valeur la plus élevée d'évacuation
@@ -61,6 +67,41 @@ def theory(N, w_porte, W_width, W_height, ListParticles, Dsortie):
     # result.set("Nombre d'individus sorties :", str(interface.getvar(name="Nbr_part_out"))
     #            + '\n' "Temps total théorique nécessaire pour sortir : " '\n' + str(t_total) + "secondes")
     return t_total
+
+###Ancien Modèle###
+# def theory(N, w_porte, W_width, W_height, ListParticles, Dsortie):
+#     ### MODELE DE TOGAWA ###
+#     W = w_porte * 2  # Width of the door
+#     D = (int(N) / (
+#             (W_width / 3779) * (
+#             W_height / 3779)))  # Density of people by meter square (1 meter = 3779 pixels don't know from where)
+#
+#     v0 = 1.3 / 3779  # (m/s) vitesse de marche par défaut et sans effet de foule
+#     vT = v0 * math.pow(D, -0.8)
+#
+#     v = []  # speed of the individuals
+#     for i in range(len(ListParticles)):
+#         v.append(ListParticles[i].speed)
+#     vAverage = mean(v)
+#
+#     F = vAverage * (W / 3779) * D  # Flux de personne (débit de pers / sec à l'ouverture)
+#     Fspe = vAverage * D  # Flux de personne (débit spécifique en m^-1.s^-1 )
+#
+#     tevac = []
+#     L = []
+#     for i in range(len(ListParticles)):
+#         L.append(Dsortie / 3779)
+#         tevac.append(((float(N) / (float(Fspe) * float(W / 3779))) + (
+#                 L[i] / vT)) * 10)  # Temps d'évacuation # LE FOIS 10 NE DOIT PAS ETRE LA
+#
+#     t_total = double(
+#         "{:.2f}".format(max(tevac)))  # Temps total de l'évacuation correspond à la valeur la plus élevée d'évacuation
+#
+#     print("Temps total théorique nécessaire pour sortir : " + str(t_total) + "secondes")
+#     # result.set("Nombre d'individus sorties :", str(interface.getvar(name="Nbr_part_out"))
+#     #            + '\n' "Temps total théorique nécessaire pour sortir : " '\n' + str(t_total) + "secondes")
+#     return t_total
+
 
 ### MODELE DE FORCE SOCIALE D'HELBING ###
 # Force sociale :
